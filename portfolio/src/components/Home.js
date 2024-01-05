811a8261bf4fd5e3a34e116c2f5f3f89c1d66be9
Youuusseff <youssefbenomrane45@gmail.com>
2024-01-05 19:53:31 +0100

    fixing nav and doing home component

--- portfolio/src/components/Home.js
+++ portfolio/src/components/Home.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import pdp from '../images/pdp.jpeg'
 
 export default function Home() {
   return (
     <div className='Home'>
-        
+      <div className='pdp-wrapper'>
+        <img className='img-pdp' src={pdp} alt='pdp'></img>
+      </div>
+      <div className='description'>
+        <p>Hello, I'm</p>
+        <h1>Youssef Benomrane</h1>
+        <h3>Frontend Developer</h3>
+        <h4>Tech Stack : React | SpringBoot </h4>
+      </div>
     </div>
   )
 }
