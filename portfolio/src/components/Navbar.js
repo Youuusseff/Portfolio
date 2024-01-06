647d8edef1ca0dec8321798a0511418ba8e58fc2
Youuusseff <youssefbenomrane45@gmail.com>
2024-01-06 02:10:14 +0100

    dropdown menu

--- portfolio/src/components/Navbar.js
+++ portfolio/src/components/Navbar.js
@@ -1,16 +1,28 @@
 import React from 'react'
 export default function Navbar() {
+  function dropDown() {
+    var x = document.getElementById("my-nav");
+    console.log(x);
+    if (x.className === "right-nav") {
+      x.className += " responsive";
+    } else {
+      x.className = "right-nav";
+    }
+  }
   return (
     <>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
       <nav className='navbar'>
         <ul className='main-nav'>
           <li>Youssef Benomrane</li>
-          <li className='right-nav'>
+          <li className='right-nav' id='my-nav'>
             <ul>
-              <li>Home</li>
-              <li>About</li>
-              <li>Projects</li>
-              <li>Contact</li>
+              <li className='dropdown-item'>Home</li>
+              <li className='dropdown-item'>About</li>
+              <li className='dropdown-item'>Projects</li>
+              <li className='dropdown-item'>Contact</li>
+              <li className="icon" onClick={()=>dropDown()}>
+                  <i class="fa fa-bars"></i></li>
             </ul>
           </li>
         </ul>
