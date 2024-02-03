f8c961d11cec717b9e82598acaff7b31914d07a0
Youuusseff <youssefbenomrane45@gmail.com>
2024-02-03 18:18:13 +0100

    fixed navbar react router

--- portfolio/src/components/home/Navbar.js
+++ portfolio/src/components/home/Navbar.js
@@ -27,11 +27,11 @@ export default function Navbar() {
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
       <nav className='navbar'>
         <ul className='main-nav'>
-          <li className='logo'><Link to="home" style={{textDecoration: 'none',color: 'inherit'}}>Youssef Benomrane</Link></li>
+          <li className='logo'><Link to="/home" style={{textDecoration: 'none',color: 'inherit'}}>Youssef Benomrane</Link></li>
           <li className='right-nav' id='my-nav'>
             <ul id='dropdown' className='dropdown'>
-              <li className='dropdown-item'><Link to="home" style={{textDecoration: 'none',color: 'inherit'}}>Home</Link></li>
-              <li className='dropdown-item'><Link to="about" style={{textDecoration: 'none',color: 'inherit', animation:"rotate 2s infinite"}} >About</Link></li>
+              <li className='dropdown-item'><Link to="/home" style={{textDecoration: 'none',color: 'inherit'}}>Home</Link></li>
+              <li className='dropdown-item'><Link to="/about" style={{textDecoration: 'none',color: 'inherit', animation:"rotate 2s infinite"}} >About</Link></li>
               <li className='dropdown-item'>Projects</li>
               <li className='dropdown-item'>Contact</li>
               <li  id="icon" className="icon" onClick={()=>dropDown()}>
