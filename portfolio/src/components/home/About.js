a315f8a6e6ad875a36ea2f6a6a580af1348aebd9
Youuusseff <youssefbenomrane45@gmail.com>
2024-02-17 23:45:47 +0100

    random

--- portfolio/src/components/home/About.js
+++ portfolio/src/components/home/About.js
@@ -1,17 +1,16 @@
-import React, { useEffect } from 'react'
-import Navbar from './Navbar'
+import React, { useEffect } from 'react';
+import Navbar from './Navbar';
 import './App.css';
 import './About.css';
 import Starfield from 'react-starfield';
 import gsap from 'gsap';
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
-
-
-
-function About() { 
+function About() {  
   return (
     <div className='About'>
-      <Starfield className='starfield'
+      <Starfield
+        className='starfield'
         starCount={2000}
         starColor={[255, 255, 255]}
         speedFactor={0.05}
@@ -21,13 +20,13 @@ function About() {
       <div className='about-wrapper'>
         <div className='details'>
           <div className='details-part'>
-            <h1 class="line-1 anim-typewriter">Learn more about me!</h1>
+            <h1 className="line-1 anim-typewriter">Learn more about me!</h1>
             <p className='scroll'>Scroll to continue</p>
           </div>
         </div>
       </div>  
     </div>
-  )
+  );
 }
 
-export default About
+export default About;
