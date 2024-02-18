903db404e883716cd7bc87dc323d760cf6e73187
Youuusseff <youssefbenomrane45@gmail.com>
2024-02-18 01:14:32 +0100

    trying to find the solution for scroll

--- portfolio/src/components/home/About.js
+++ portfolio/src/components/home/About.js
@@ -6,7 +6,22 @@ import Starfield from 'react-starfield';
 import gsap from 'gsap';
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
-function About() {  
+function About() { 
+  gsap.timeline({
+    defaults: {
+      duration: 6000 },
+    scrollTrigger: {
+      trigger: '.details',
+      scrub: true,
+      start: 0,
+      end: '+=3000',
+      pin: '.about-wrapper'
+    }
+  })
+    .to(".details-part", { opacity: 0, y: -20 })
+    .from(".details-part2", { opacity: 0, y: 40})
+    .to(".details-part2", { opacity: 0, delay: 600})
+
   return (
     <div className='About'>
       <Starfield
@@ -19,9 +34,12 @@ function About() {
       <Navbar/>
       <div className='about-wrapper'>
         <div className='details'>
-          <div className='details-part'>
+          <div className='details-part'style={{marginBottom:'100vh'}}>
             <h1 className="line-1 anim-typewriter">Learn more about me!</h1>
             <p className='scroll'>Scroll to continue</p>
+          </div>
+          <div className='details-part2'>
+            <h1 style={{color:'white'}}>Hi</h1>
           </div>
         </div>
       </div>  
